2fd888f3898da107e9c16c33ed3569240d070070
Rioton <riotonchen@gmail.com>
2023-02-22 21:52:21 +0800

    create-a-hero-interface

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,26 +1,17 @@
-// import { Component } from '@angular/core';
-
-// @Component({
-//   selector: 'app-heroes',
-//   templateUrl: './heroes.component.html',
-//   styleUrls: ['./heroes.component.css']
-// })
-// export class HeroesComponent {
-
-// }
 import { Component,OnInit } from '@angular/core';
-
-
-
+import { Hero } from '../heroes';
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  selector: 'app-heroes',  //  元件的 CSS 元素選擇器
+  templateUrl: './heroes.component.html', // 元件範本檔案的位置。
+  styleUrls: ['./heroes.component.css'] //元件私有 CSS 樣式表文件的位置
 })
 
 export class HeroesComponent implements OnInit {
+  hero: Hero = {
+    id: 1,
+    name: 'Windsrorm'
+  };
   constructor() { }
   ngOnInit(): void {
-
   }
 }
