c4990020db4a43f91f1c614d8232080fb4aed00e
Rioton <riotonchen@gmail.com>
2023-02-22 21:22:29 +0800

    The hero editor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-tour-of-heroes';
+  // title = 'angular-tour-of-heroes';
+  title = 'Tour of Apple';
+  content = 'small apple';
 }
