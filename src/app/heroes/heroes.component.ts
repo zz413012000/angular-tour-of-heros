22cee90946d6fb595f053682a84ab31d84acfdef
Rioton <riotonchen@gmail.com>
2023-02-25 19:02:48 +0800

    Add service_Update HeroesComponent

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component,OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { Apple } from '../apple';
+import { HeroService } from '../hero.service';
 
 import { HEROES } from '../mock-heroes';
 @Component({
@@ -10,13 +10,16 @@ import { HEROES } from '../mock-heroes';
 })
 
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
-
+  // heroes = HEROES;
   selectedHero!: Hero;
+  heroes: Hero[] = [];
 
-  constructor() { }
-
+  constructor(private heroService: HeroService) { }
   ngOnInit(): void {
+    this.getHeroes();
+  }
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes()
   }
 
   onSelect(hero: Hero): void {
