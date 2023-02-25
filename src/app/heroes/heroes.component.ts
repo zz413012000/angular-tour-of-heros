6d1fc9c68df09257b5d28767cbb0d227736304de
Rioton <riotonchen@gmail.com>
2023-02-25 20:56:56 +0800

    Add service_Create MessagesComponent

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -19,7 +19,9 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes()
+    // this.heroes = this.heroService.getHeroes()
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 
   onSelect(hero: Hero): void {
