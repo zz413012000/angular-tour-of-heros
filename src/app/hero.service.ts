f7a95bd29880c0017c61f0e0cce513fd82543b89
zz413012000 <zz413012000@gmail.com>
2023-03-23 20:56:08 +0800

    新增導航_支援路由的 HeroDetailComponent-新增 HeroService.getHero() (not Done!)

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -26,8 +26,6 @@ export class HeroService {
   //  return of(hero);
   //}
     getHero(id: number): Observable<Hero> {
-    // For now, assume that a hero with the specified `id` always exists.
-    // Error handling will be added in the next step of the tutorial.
     const hero = HEROES.find(h => h.id === id)!;
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(hero);
